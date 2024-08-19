e7812e73c6f6eed0fd66509631f49f41e50e22b7
EC2 Default User <ec2-user@ip-172-31-1-162.ap-northeast-1.compute.internal>
2024-08-19 13:00:36 +0000

    [add] meal+training-view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,6 +5,8 @@ class Admin::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+    @meals = @customer.meals
+    @trainings = @customer.trainings
   end
 
   def destroy
