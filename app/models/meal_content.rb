6d76b3215cc6b856257949da84d94eaaea93814e
EC2 Default User <ec2-user@ip-172-31-1-162.ap-northeast-1.compute.internal>
2024-08-21 09:13:30 +0000

    [add] meal+training-required

--- app/models/meal_content.rb
+++ app/models/meal_content.rb
@@ -1,7 +1,7 @@
 class MealContent < ApplicationRecord
   belongs_to :meal
 
-  validates :meal_summary, presence: true
+  validates :meal_summary, presence: { message: "食事概要を入力してください" }
   validates :eat_meal, presence: true
   validates :kcal, numericality: { greater_than: 0 }
 
