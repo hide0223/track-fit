a0d907d919464980466e3f1a18945b51db201317
EC2 Default User <ec2-user@ip-172-31-1-162.ap-northeast-1.compute.internal>
2024-08-26 08:30:58 +0000

    [add] testcustomer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -19,7 +19,7 @@ class Customer < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  # validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   #validates :introduction, length: { maximum: 50 }
 
   def self.guest
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,67 @@ Admin.create!(
    email: 'admin@admin',
    password: 'adminadmin'
 )
+
+alice = Customer.find_or_create_by!(email: "alice@example.com") do |customer|
+ customer.name = "Alice"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer1.jpg"), filename:"sample-customer1.jpg")
+end
+
+carol = Customer.find_or_create_by!(email: "carol@example.com") do |customer|
+ customer.name = "Carol"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer2.jpg"), filename:"sample-customer2.jpg")
+end
+
+bob = Customer.find_or_create_by!(email: "bob@example.com") do |customer|
+ customer.name = "Bob"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer3.jpg"), filename:"sample-customer3.jpg")
+end
+
+dave = Customer.find_or_create_by!(email: "dave@example.com") do |customer|
+ customer.name = "Dave"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer4.jpg"), filename:"sample-customer4.jpg")
+end
+
+テスト = Customer.find_or_create_by!(email: "test@example.com") do |customer|
+ customer.name = "テスト"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer5.jpg"), filename:"sample-customer5.jpg")
+end
+
+トラック = Customer.find_or_create_by!(email: "track@example.com") do |customer|
+ customer.name = "トラック"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer6.jpg"), filename:"sample-customer6.jpg")
+end
+
+フィット = Customer.find_or_create_by!(email: "fit@example.com") do |customer|
+ customer.name = "フィット"
+ customer.password = "password"
+ customer.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-customer7.jpg"), filename:"sample-customer7.jpg")
+end
+
+
+Meal.find_or_create_by!(
+  customer_id: 7,
+  body: "朝ごはんは卵焼きが好きです",
+  body_weight: 68.0,
+  category: "朝食"
+)
+
+MealContent.find_or_create_by!(
+  meal_id: 7,
+  meal_summary: "主食",
+  eat_meal: "おにぎり",
+  kcal: 100
+)
+
+MealContent.find_or_create_by!(
+  meal_id: 7,
+  meal_summary: "主菜",
+  eat_meal: "卵焼き",
+  kcal: 100
+)
